ebb5f65f2b066eea1e96d508776eeaac8cb94ff6
Ronen Narkis <narkisr@gmail.com>
2013-07-26 19:43:34 +0300

    license header and ns doc

--- src/subs/core.clj
+++ src/subs/core.clj
@@ -1,5 +1,23 @@
+(comment
+   Celestial, Copyright 2012 Ronen Narkis, narkisr.com
+   Licensed under the Apache License,
+   Version 2.0  (the "License") you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.)
+
 (ns subs.core
-  ""
+  " Substantiation is an opinionated simple nested map validation framework:
+    * Predicates and description kept seperate.
+    * Validations map description sturcture follows validated input.
+    * Pure datastructures to describe validations.
+    * Composeability of validations should be trivial.
+    * Validation predicates scope is limited (can only access the checked value).
+    * High level decisions such as when to activate a group of validations should happen on upper layer.
+    * Non strict, only described items checked. "
   (:use 
     [slingshot.slingshot :only  (throw+)]
     [clojure.core.strint :only (<<)] 
