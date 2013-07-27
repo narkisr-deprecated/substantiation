d3078aaf4c92851a7079658beaf7ba1feab0cb1c
Ronen Narkis <narkisr@gmail.com>
2013-07-27 05:24:57 +0300

    version up

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject substantiation "0.0.2"
+(defproject substantiation "0.0.3"
   :description "An opinionated nested maps validations framework"
   :url "https://github.com/narkisr/substantiation"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
