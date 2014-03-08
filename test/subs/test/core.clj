98dfc7bdde4e8b5b560e136451e5936521f50093
narkisr <narkisr@gmail.com>
2014-03-08 21:08:07 +0200

    enabling test back

--- test/subs/test/core.clj
+++ test/subs/test/core.clj
@@ -12,62 +12,62 @@
   (validate! {:machine {:used "1"}} {:machine {:used #{:Boolean :required}}}) =>
       {:machine {:used "must be a boolean"}})
 
-;; (fact "non empty validations"
-;;   (validate! {:machine {:ip ""}} {:machine {:ip #{:String! :required}}}) =>
-;;       {:machine {:ip '("must be a non empty string")}}
-;;   (validate! {:machine {:names ""}} {:machine {:names #{:Vector! :required}}}) =>
-;;       {:machine {:names '("must be a non empty vector")}}
-;;   (validate! {:machine {:templates {}}} {:machine {:templates #{:Map! :required}}}) =>
-;;       {:machine {:templates '("must be a non empty map")}})
-;;
-;; (fact "all just fine" 
-;;   (validate! {:machine {:ip "1.2.3.4" :used false}} {:machine {:ip #{:String :required} :used #{:Boolean}}}) => {})
-;;
-;; (fact "booleans" 
-;;    (validate! {:foo false} {:foo #{:Boolean :required}}) => {}
-;;    (validate! {:foo true} {:foo #{:Boolean :required}}) => {}
-;;    (validate! {:foo "true"} {:foo #{:Boolean :required}}) => {:foo '("must be a boolean")})
-;;
-;; (fact "order does not matter"
-;;   (validate! {:machine {:ip 1}} {:machine {:ip #{:required :String}}})) => {:machine {:ip '("must be a string")}}
-;;
-;; (fact "missing custom validation"
-;;   (let [v {:machine {:ip #{:String :required} :names #{:required} :level #{:level}}}] 
-;;     (validate! {:machine {:names {:foo 1} :ip 1}} v) => (throws ExceptionInfo)))
-;;
-;; (fact "composition"
-;;   (let [ v1 {:machine {:ip #{:String :required} :names #{:Vector}} :vcenter {:pool #{:String}}} 
-;;          v2 {:machine {:ip #{:String :required} :names #{:required} }}]
-;;     (validate! {:machine {:names {:foo 1} :ip 1}} (combine v2 v1)) => 
-;;          {:machine {:ip '("must be a string"), :names '("must be a vector")}}  ))  
-;;
-;; (fact "with error" 
-;;    (validate! {:machine {:ip 1}} {:machine {:ip #{:String :required}}} :error ::non-vaild-machine) => (throws ExceptionInfo))
-;;
-;; (validation :named-node* (every-kv {:ip #{:required} :names #{:name*}}))
-;;
-;; (validation :node* (every-kv {:ip #{:required}}))
-;;
-;; (validation :name* (every-v #{:String :required}))
-;;
-;; (fact "every item validations"
-;;
-;;   (validate! {:proxmox {:nodes {:master {} :slave {}}}} {:proxmox {:nodes #{:node*}}}) => 
-;;      {:proxmox {:nodes '(({:master {:ip ("must be present")}} {:slave {:ip ("must be present")}}))}}
-;;
-;;   (validate! {:proxmox {:nodes {:master {:ip 123}}}} {:proxmox {:nodes #{:node*}}}) => {}
-;;
-;;   (validate! {:names [1 "1"]} {:names #{:name*}}) =>  {:names '(({0 ("must be a string")}))}
-;;
-;;   (validate! {:names ["1"]} {:names #{:name*}}) =>  {}
-;;
-;;   (validate! {:names '("1" "2" 3)} {:names #{:name* :Vector}}) => '{:names (({2 ("must be a string")}) "must be a vector")}
-;;
-;;   (validate! {:nodes {:master {} :slave {:names [1]}}} {:nodes #{:named-node*}}) => 
-;;        {:nodes '(({:master {:ip ("must be present")}} {:slave {:ip ("must be present") :names (({0 ("must be a string")}))}}))}
-;;
-;;   (validate! {0 1} {0 #{:String}}) => {0 '("must be a string")})
-;;
-;; (fact "ANY key validations"
-;;    (validate! {:aws {:limits 1} :proxmox {}} {:subs.core/ANY {:limits #{:required :Integer}}}) => {} 
-;;       )
+(fact "non empty validations"
+  (validate! {:machine {:ip ""}} {:machine {:ip #{:String! :required}}}) =>
+      {:machine {:ip "must be a non empty string"}}
+  (validate! {:machine {:names ""}} {:machine {:names #{:Vector! :required}}}) =>
+      {:machine {:names "must be a non empty vector"}}
+  (validate! {:machine {:templates {}}} {:machine {:templates #{:Map! :required}}}) =>
+      {:machine {:templates "must be a non empty map"}})
+
+(fact "all just fine" 
+  (validate! {:machine {:ip "1.2.3.4" :used false}} {:machine {:ip #{:String :required} :used #{:Boolean}}}) => {})
+
+(fact "booleans" 
+   (validate! {:foo false} {:foo #{:Boolean :required}}) => {}
+   (validate! {:foo true} {:foo #{:Boolean :required}}) => {}
+   (validate! {:foo "true"} {:foo #{:Boolean :required}}) => {:foo "must be a boolean"})
+
+(fact "order does not matter"
+  (validate! {:machine {:ip 1}} {:machine {:ip #{:required :String}}})) => {:machine {:ip '("must be a string")}}
+
+(fact "missing custom validation"
+  (let [v {:machine {:ip #{:String :required} :names #{:required} :level #{:level}}}] 
+    (validate! {:machine {:names {:foo 1} :ip 1}} v) => (throws ExceptionInfo)))
+
+(fact "composition"
+  (let [ v1 {:machine {:ip #{:String :required} :names #{:Vector}} :vcenter {:pool #{:String}}} 
+         v2 {:machine {:ip #{:String :required} :names #{:required} }}]
+    (validate! {:machine {:names {:foo 1} :ip 1}} (combine v2 v1)) => 
+         {:machine {:ip "must be a string", :names "must be a vector"}}  ))  
+
+(fact "with error" 
+   (validate! {:machine {:ip 1}} {:machine {:ip #{:String :required}}} :error ::non-vaild-machine) => (throws ExceptionInfo))
+
+(validation :named-node* (every-kv {:ip #{:required} :names #{:name*}}))
+
+(validation :node* (every-kv {:ip #{:required}}))
+
+(validation :name* (every-v #{:String :required}))
+
+(fact "every item validations"
+
+  (validate! {:proxmox {:nodes {:master {} :slave {}}}} {:proxmox {:nodes #{:node*}}}) => 
+     {:proxmox {:nodes '(({:master {:ip "must be present"}} {:slave {:ip "must be present"}}))}}
+
+  (validate! {:proxmox {:nodes {:master {:ip 123}}}} {:proxmox {:nodes #{:node*}}}) => {}
+
+  (validate! {:names [1 "1"]} {:names #{:name*}}) =>  {:names '(({0 "must be a string"}))}
+
+  (validate! {:names ["1"]} {:names #{:name*}}) =>  {}
+
+  (validate! {:names '("1" "2" 3)} {:names #{:name* :Vector}}) => '{:names (({2 "must be a string"}) "must be a vector")}
+
+  (validate! {:nodes {:master {} :slave {:names [1]}}} {:nodes #{:named-node*}}) => 
+       {:nodes '(({:master {:ip "must be present"}} {:slave {:ip "must be present" :names (({0 "must be a string"}))}}))}
+
+  (validate! {0 1} {0 #{:String}}) => {0 "must be a string"})
+
+#_(fact "ANY key validations"
+   (validate! {:aws {:limits 1} :proxmox {}} {:subs.core/ANY {:limits #{:required :Integer}}}) => {} 
+      )
