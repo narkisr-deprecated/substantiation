8055c4bf5e4d8722f18521b2d26920c23fa5cb88
narkisr <narkisr@gmail.com>
2014-03-11 16:22:56 +0200

    version up with better error handling

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject substantiation "0.1.1"
+(defproject substantiation "0.1.2"
   :description "An opinionated nested maps validations framework"
   :url "https://github.com/narkisr/substantiation"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
