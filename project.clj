f86976fff654bd88d61dd0a63aa85d1010d47795
ronen <narkisr@gmail.com>
2015-06-30 17:56:08 +0300

    latest deps

--- project.clj
+++ project.clj
@@ -2,16 +2,16 @@
   :description "An opinionated nested maps validations framework"
   :url "https://github.com/narkisr/substantiation"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"] 
+  :dependencies [[org.clojure/clojure "1.6.0"] 
                  [slingshot "0.10.3"] 
-                 [org.clojure/core.incubator "0.1.2"]]
+                 [org.clojure/core.incubator "0.1.3"]]
 
   :exclusions [org.clojure/clojure]
 
   :profiles {
    :dev {
       :source-paths  ["dev" "src"]
-      :dependencies [[org.clojure/tools.trace "0.7.5"] [midje "1.5.1"]]
+      :dependencies [[org.clojure/tools.trace "0.7.8"] [midje "1.6.3"]]
       :set-version {
         :updates [{:path "README.md" :search-regex #"\"\d+\.\d+\.\d+\""}]
       }
@@ -25,7 +25,7 @@
             ["midje" ":filter" "-integration"] 
             }
 
-  :plugins  [[jonase/eastwood "0.0.2"] [lein-midje "3.0.0"] [lein-ancient "0.4.2"]
+  :plugins  [[jonase/eastwood "0.0.2"] [lein-midje "3.0.0"] [lein-ancient "0.6.7"]
              [lein-tag "0.1.0"] [lein-set-version "0.3.0"] [codox "0.6.4"]]
 
  
